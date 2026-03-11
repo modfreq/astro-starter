f3f67b532dec7bc785a11385b566be36550a2874
Alex Brown <alex@alexbrown.io>
2026-03-11 15:34:11 -0700

    fix: configure TypeScript parser for Astro ESLint
    Add @typescript-eslint/parser so ESLint can parse TypeScript syntax
    (interfaces, type annotations) in .astro frontmatter blocks.

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,18 @@
 import eslintPluginAstro from "eslint-plugin-astro";
 import eslintConfigPrettier from "eslint-config-prettier";
+import tsParser from "@typescript-eslint/parser";
 
 export default [
   ...eslintPluginAstro.configs.recommended,
   eslintConfigPrettier,
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parserOptions: {
+        parser: tsParser,
+      },
+    },
+  },
   {
     ignores: ["dist/", ".astro/", "node_modules/"],
   },
